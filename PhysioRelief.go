1ca5099b66ba9a26ea7f87c196eaf3fd4f44379b
Shubham Navle <120539849+snavale230@users.noreply.github.com>
2024-06-12 21:54:55 +0530

    Update PhysioRelief.go

--- PhysioRelief.go
+++ PhysioRelief.go
@@ -7,18 +7,6 @@ import (
 	"github.com/spf13/viper"
 )
 
-// ViperInitialization initializes Viper by adding the config path, setting the config name, and reading the config from the file. If there is an error reading the config, it logs it
-func ViperInitialization() error {
-	viper.AddConfigPath("/app")
-	viper.SetConfigName("config")
-	err := viper.ReadInConfig()
-	if err != nil {
-		log.Error().Err(err).Msg("viper config not found")
-		return err
-	}
-	return nil
-}
-
 func main() {
 	server := &helpers.Server{}
 	err := server.InitializeRoutes()
